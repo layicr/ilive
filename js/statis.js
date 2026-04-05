b47d4cc297e46d28d8cf8099f4b48eb69eaadfcc
layicr <liyicong768@163.com>
2026-04-05 21:45:45 +0800

    合并文件

--- js/statis.js
+++ js/statis.js
@@ -34,5 +34,5 @@ var _hmt = _hmt || [];
     document.head.appendChild(firstScript);
 })();
 
-// 51la统计
+// 51la
 !function (p) { "use strict"; !function (t) { var s = window, e = document, i = p, c = "".concat("https:" === e.location.protocol ? "https://" : "http://", "sdk.51.la/js-sdk-pro.min.js"), n = e.createElement("script"), r = e.getElementsByTagName("script")[0]; n.type = "text/javascript", n.setAttribute("charset", "UTF-8"), n.async = !0, n.src = c, n.id = "LA_COLLECT", i.d = n; var o = function () { s.LA.ids.push(i) }; s.LA ? s.LA.ids && o() : (s.LA = p, s.LA.ids = [], o()), r.parentNode.insertBefore(n, r) }() }({ id: "L8VlyZ1HkDS0E3VO", ck: "L8VlyZ1HkDS0E3VO" });
